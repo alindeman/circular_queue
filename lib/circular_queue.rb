26f53fb43423739e8afc055b4aa594e89ccf94f5
Andy Lindeman <andy@highgroove.com>
2011-10-17 12:00:23 -0400

    Queue can have items inserted into it

--- lib/circular_queue.rb
+++ lib/circular_queue.rb
@@ -1,7 +1,38 @@
 class CircularQueue
   attr_reader :capacity
 
+  attr_reader :size
+  alias :length :size
+
   def initialize(capacity)
     @capacity = capacity
+    @data     = Array.new(capacity)
+
+    @size     = 0
+    @front    = 0
+  end
+
+  def enq(item)
+    @data[back] = item
+    @size += 1
+  end
+  alias :<<   :enq
+  alias :push :enq
+
+  def deq
+    item = @data[@front]
+
+    @size  -= 1
+    @front += 1
+
+    item
+  end
+  alias :shift :deq
+  alias :pop   :deq
+
+  private
+
+  def back
+    (@front + @size) % @capacity
   end
 end
--- spec/circular_queue_spec.rb
+++ spec/circular_queue_spec.rb
@@ -1,9 +1,24 @@
 require "spec_helper"
 
 describe CircularQueue do
+  let(:capacity) { 5 }
+  subject        { described_class.new(capacity) }
+
   describe "initialization" do
     it "can be initialized with a capacity" do
-      described_class.new(5).capacity.should == 5
+      subject.capacity.should == capacity
+    end
+  end
+
+  describe "adding items" do
+    it "accepts new items" do
+      subject.enq(1234)
+      subject.deq.should == 1234
+    end
+
+    it "increases its size when a new item is added" do
+      subject.enq(1234)
+      subject.size.should == 1
     end
   end
 end
