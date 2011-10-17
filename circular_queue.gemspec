5eac72bf3876c2300ae2f144d94423fdd3328b25
Andy Lindeman <andy@highgroove.com>
2011-10-17 13:59:32 -0400

    Adds documentation

--- circular_queue.gemspec
+++ circular_queue.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
 
   s.add_development_dependency "rspec", "~>2.7.0"
+  s.add_development_dependency "yard"
 end
--- lib/circular_queue.rb
+++ lib/circular_queue.rb
@@ -1,11 +1,38 @@
 require "thread"
 
+# A thread-safe queue with a size limitation. When more elements than the
+# capacity are added, the queue either loops back on itself (removing the
+# oldest elements first) or raises an error (if `enq!` is used).
+#
+# Useful for streaming data where keeping up with real-time is more important
+# than consuming every message if load rises and the queue backs up.
+#
+# Exposes the same interface as the `Queue` from the Ruby stdlib.
+#
+# Example:
+#
+#     # Capacity of 3
+#     q = CircularQueue.new(3)
+#
+#     q << 1 # => [1]
+#     q << 2 # => [1, 2]
+#     q << 3 # => [1, 2, 3]
+#
+#     # Elements are replaced when the queue reaches capacity
+#     q << 4 # => [4, 2, 3]
+#     q << 5 # => [4, 5, 3]
 class CircularQueue
+  # Returns the maximum number of elements that can be enqueued
+  # @return [Integer]
   attr_reader :capacity
 
+  # Returns the number of elements in the queue
+  # @return [Integer]
   attr_reader :size
   alias length size
 
+  # Creates a new queue of the specified capacity
+  # @param [Integer] the maximum capacity of the queue
   def initialize(capacity)
     @capacity = capacity
     @data     = Array.new(capacity)
@@ -16,6 +43,8 @@ class CircularQueue
     clear
   end
 
+  # Adds an item to the queue
+  # @param [Object] item to add
   def enq(item)
     @mutex.synchronize do
       enq_item(item)
@@ -25,6 +54,9 @@ class CircularQueue
   alias <<   enq
   alias push enq
 
+  # Adds an item to the queue, raising an error if the queue is full
+  # @param [Object] item to add
+  # @raise [ThreadError] queue is full
   def enq!(item)
     @mutex.synchronize do
       raise ThreadError.new("Queue is full") if full?
@@ -35,6 +67,10 @@ class CircularQueue
   end
   alias push! enq!
 
+  # Removes an item from the queue
+  # @param [Boolean] true to raise an error if the queue is empty; otherwise,
+  #                  waits for an item to arrive from another thread
+  # @raise [ThreadError] non_block was true and the queue was empty
   def deq(non_block = false)
     @mutex.synchronize do
       while true
@@ -52,6 +88,7 @@ class CircularQueue
   alias shift deq
   alias pop   deq
 
+  # Removes all items from the queue
   def clear
     @mutex.synchronize do
       @size  = 0
@@ -60,14 +97,20 @@ class CircularQueue
     end
   end
 
+  # Returns whether the queue is empty
+  # @return [Boolean] queue is empty
   def empty?
     @size == 0
   end
 
+  # Returns whether the queue is full
+  # @return [Boolean] queue is full
   def full?
     @size == @capacity
   end
 
+  # Returns the number of threads waiting for items to arrive in the queue
+  # @return [Integer] number of threads waiting
   def num_waiting
     @waiting.length
   end
