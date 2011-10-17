7cd9162c0d0ca90502d7392425fc4e43fc298fca
Andy Lindeman <andy@highgroove.com>
2011-10-17 12:31:22 -0400

    Enforces thread safety

--- lib/circular_queue.rb
+++ lib/circular_queue.rb
@@ -10,45 +10,55 @@ class CircularQueue
     @capacity = capacity
     @data     = Array.new(capacity)
 
+    @mutex    = Mutex.new
+
     clear
   end
 
   def enq(item)
-    @data[@back] = item
+    @mutex.synchronize do
+      @data[@back] = item
 
-    @size += 1 unless full?
+      @size += 1 unless full?
 
-    @back += 1
-    @back %= @capacity
+      @back += 1
+      @back %= @capacity
+    end
   end
   alias :<<   :enq
   alias :push :enq
 
   def enq!(item)
-    raise ThreadError.new("Queue is full") if full?
-    enq(item)
+    @mutex.synchronize do
+      raise ThreadError.new("Queue is full") if full?
+      enq(item)
+    end
   end
   alias :push! :enq!
 
   def deq
-    raise ThreadError.new("Queue is empty") if empty?
+    @mutex.synchronize do
+      raise ThreadError.new("Queue is empty") if empty?
 
-    item = @data[@front]
+      item = @data[@front]
 
-    @size  -= 1
+      @size  -= 1
 
-    @front += 1
-    @front %= @capacity
+      @front += 1
+      @front %= @capacity
 
-    item
+      item
+    end
   end
   alias :shift :deq
   alias :pop   :deq
 
   def clear
-    @size  = 0
-    @front = 0
-    @back  = 0
+    @mutex.synchronize do
+      @size  = 0
+      @front = 0
+      @back  = 0
+    end
   end
 
   def empty?
